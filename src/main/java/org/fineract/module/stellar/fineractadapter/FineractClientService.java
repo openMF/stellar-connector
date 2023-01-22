720e578510f55629e6357f3ad877206fac5ad4d8
Vic Romero <victor.romero@fintecheando.mx>
2023-01-22 01:26:11 -0600

    Fix for Stellar Connector

--- src/main/java/org/fineract/module/stellar/fineractadapter/FineractClientService.java
+++ src/main/java/org/fineract/module/stellar/fineractadapter/FineractClientService.java
@@ -25,7 +25,7 @@ import retrofit2.http.Query;
 public interface FineractClientService {
   @GET("/clients/{id}")
   Client findClient(@Header("Authorization") String authorization,
-      @Header("X-Mifos-Platform-TenantId") String tenantIdentifier,
+      @Header("Fineract-Platform-TenantId") String tenantIdentifier,
       @Path("id") final long id);
 
   @POST("/savingsaccounts/{accountId}/transactions")
